ee9da29d94454fe17167392c5cb23cf8d841969b
Lu <lumingxian888@gmail.com>
2026-06-29 06:42:49 +0800

    Update zzz.js

--- zzz.js
+++ zzz.js
@@ -1,1 +1,18 @@
+var DateStr = (function(){
+  var d = new Date();
+  var pad = function(n) { return (n < 10 ? '0' : '') + n; };
+  return d.getFullYear() + '-' + pad(d.getMonth() + 1) + '-' + pad(d.getDate());
+})();
+
+var WeekStr = (function(){
+  var weeks = ['日', '一', '二', '三', '四', '五', '六'];
+  var index = new Date().getDay(); // 取得 0 (日) 到 6 (六)
+  return '(' + weeks[index] + ')';
+})();
+
+var TimeStr = (function(){
+  var d = new Date();
+  var pad = function(n) { return (n < 10 ? '0' : '') + n; };
+  return pad(d.getHours()) + ':' + pad(d.getMinutes()) + ':' + pad(d.getSeconds());
+})();
 
